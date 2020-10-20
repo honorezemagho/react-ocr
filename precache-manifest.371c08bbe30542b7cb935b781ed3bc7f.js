011667e66a1be52b76aadbfc5b945e20f333ee0d
honorezemagho <honorezemagho@gmail.com>
2020-10-20 11:24:20 +0000

    Updates

---
+++ precache-manifest.371c08bbe30542b7cb935b781ed3bc7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b7929a9ef813df7adf04ec0fe9d8e51",
+    "url": "/react-ocr/index.html"
+  },
+  {
+    "revision": "84afed04d354daa65014",
+    "url": "/react-ocr/static/css/2.e28dc06a.chunk.css"
+  },
+  {
+    "revision": "4814eb7a988af6a82347",
+    "url": "/react-ocr/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "84afed04d354daa65014",
+    "url": "/react-ocr/static/js/2.89b96aa5.chunk.js"
+  },
+  {
+    "revision": "0e80994c58ddf756d1183e54b669b70d",
+    "url": "/react-ocr/static/js/2.89b96aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4814eb7a988af6a82347",
+    "url": "/react-ocr/static/js/main.ad139656.chunk.js"
+  },
+  {
+    "revision": "cfa004eea214e479b9d3",
+    "url": "/react-ocr/static/js/runtime-main.7d905dfe.js"
+  }
+]);
